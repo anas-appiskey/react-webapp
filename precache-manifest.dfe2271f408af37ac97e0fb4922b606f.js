25f8000ee425aa777c9789990d47adfcba3c3787
anasali2193 <anasali2193@gmail.com>
2020-03-21 23:00:05 +0500

    Updates

---
+++ precache-manifest.dfe2271f408af37ac97e0fb4922b606f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ea2de2c6b92638003dfef01127e9ef",
+    "url": "/anasali2193/react-webapp/index.html"
+  },
+  {
+    "revision": "7c13ad940cb477db7bfb",
+    "url": "/anasali2193/react-webapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3889526eb53834091d0f",
+    "url": "/anasali2193/react-webapp/static/js/2.055218eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anasali2193/react-webapp/static/js/2.055218eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c13ad940cb477db7bfb",
+    "url": "/anasali2193/react-webapp/static/js/main.ec000d2e.chunk.js"
+  },
+  {
+    "revision": "5b98bf4f30be3b1789fc",
+    "url": "/anasali2193/react-webapp/static/js/runtime-main.e568ec36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anasali2193/react-webapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
